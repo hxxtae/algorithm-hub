4d230d24e9ec9f10e4088ea3ce1fbe239e64302b
Heetae Kim <fkdlxmfkdl@gmail.com>
2024-04-06 10:11:19 +0900

    [Silver III] Title: 통계학, Time: 452 ms, Memory: 57896 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+import sys
+read = sys.stdin.readline
+
+N = int(read().rstrip())
+ARR = [int(read().rstrip()) for _ in range(N)]
+
+# 산술평균: N개의 수들의 합을 N으로 나눈 값
+print(round(sum(ARR) / N))
+
+# 중앙값: N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
+print(sorted(ARR)[N//2])
+
+# 최빈값: N개의 수들 중 가장 많이 나타나는 값
+obj = defaultdict(int)
+for i in range(N):
+  obj[ARR[i]] += 1
+
+maxCnt = max(obj.values())
+objFilter = [*filter(lambda x: maxCnt == x[1], sorted(obj.items(), key=lambda item: (item[1], item[0]), reverse=True))]
+objFilter = sorted(objFilter, key=lambda x: x[0])
+if len(objFilter) == 1:
+  print(objFilter[0][0])
+else: 
+  print(objFilter[1][0])
+
+# 범위: N개의 수들 중 최댓값과 최솟값의 차이
+print(max(ARR) - min(ARR))

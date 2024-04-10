a3dff83f921c57a99bd667d2ac2abb772994a07d
Heetae Kim <fkdlxmfkdl@gmail.com>
2024-04-10 20:21:13 +0900

    [Silver IV] Title: 수 찾기, Time: 696 ms, Memory: 49140 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,30 @@
+from heapq import *
+import sys
+read = sys.stdin.readline
+
+N = int(read().rstrip())
+ARR_N = [*map(int, read().rstrip().split())]
+M = int(read().rstrip())
+ARR_M = [*map(int, read().rstrip().split())]
+
+ARR_N.sort()
+
+for i, num in enumerate(ARR_M):
+  start = 0
+  end = N - 1
+  check = True
+
+  while start <= end:
+    half = (start + end) // 2
+    if num == ARR_N[half]:
+      check = False
+      print(1)
+      break
+    
+    if num > ARR_N[half]:
+      start = half + 1
+    else:
+      end = half - 1
+  
+  if check:
+    print(0)

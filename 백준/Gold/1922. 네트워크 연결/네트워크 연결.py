b95c7bfcfd28d13ea023cce499f95114f4601f98
Heetae Kim <fkdlxmfkdl@gmail.com>
2024-04-11 14:29:24 +0900

    [Gold IV] Title: 네트워크 연결, Time: 320 ms, Memory: 54732 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,30 @@
+import sys
+read = sys.stdin.readline
+
+N = int(read().rstrip())
+M = int(read().rstrip())
+GRAPH = [[*map(int, read().rstrip().split())] for _ in range(M)]
+
+graph = sorted(GRAPH, key=lambda x: x[2])
+parent = [i for i in range(N+1)]
+
+def getParent(x, parent):
+  return x if parent[x] == x else getParent(parent[x], parent)
+
+def unionParent(x, y, parent):
+  a, b = [getParent(x, parent), getParent(y, parent)]
+  if a < b: parent[b] = a
+  else: parent[a] = b
+
+def findParent(x, y, parent):
+  a, b = [getParent(x, parent), getParent(y, parent)]
+  if a == b: return True
+  return False
+
+answer = 0
+for a, b, val in graph:
+  if not findParent(a, b, parent):
+    unionParent(a, b, parent)
+    answer += val
+
+print(answer)

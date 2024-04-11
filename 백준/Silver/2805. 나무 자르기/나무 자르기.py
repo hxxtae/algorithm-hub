765aebe5e243ff7cb8a7d71d364b7a8f1374917b
Heetae Kim <fkdlxmfkdl@gmail.com>
2024-04-11 18:19:56 +0900

    [Silver II] Title: 나무 자르기, Time: 4576 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,26 +1,24 @@
 import sys
 read = sys.stdin.readline
 
-N, M = map(int, read().rstrip().split(' '))
-TREE = [*map(int, read().rstrip().split(' '))]
+N, M = map(int, read().rstrip().split())
+TREES = [*map(int, read().rstrip().split())]
 
-treeMin = 0
-treeMax = max(TREE)
-treeHalf = 0
-lengthSum = 0
+TREES.sort()
+start = 0
+end = TREES[-1]
 
-while treeMin <= treeMax:
-  treeHalf = (treeMax + treeMin) // 2
-  for num in TREE:
-    if num >= treeHalf:
-      lengthSum += (num - treeHalf)
+while start <= end:
+  half = (start + end) // 2
+  length = 0
+
+  for tree in TREES:
+    if tree <= half: continue
+    length += (tree - half)
   
-  if lengthSum >= M:
-    treeMin = treeHalf + 1
-  
-  if lengthSum < M:
-    treeMax = treeHalf - 1
+  if length >= M:
+    start = half + 1
+  else:
+    end = half - 1
 
-  lengthSum = 0
-
-print(treeMax)
+print(end)

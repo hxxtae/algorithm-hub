35e65cbdd7dc7eedf4e2c2d5a8f2659c75c39c65
Heetae Kim <fkdlxmfkdl@gmail.com>
2024-05-04 12:15:29 +0900

    [Gold V] Title: 택배 배송, Time: 2744 ms, Memory: 58280 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+read = sys.stdin.readline
+
+N, M = map(int, read().rstrip().split())
+GRAPH = [[*map(int, read().rstrip().split())] for _ in range(M)]
+
+# 1. Set graph
+graph = [[] for _ in range(N+1)]
+for a, b, dist in GRAPH:
+  graph[a].append([dist, b])
+  graph[b].append([dist, a])
+
+# 2. Set Distance
+distance = [float('inf') for _ in range(N+1)]
+distance[1] = 0
+
+# 3. BFS
+queue = deque([[0, 1]]) # [dist, to]
+while queue:
+  dist, to = queue.popleft()
+  
+  if distance[to] < dist: continue
+  
+  for step_dist, step_to in graph[to]:
+    new_dist = dist + step_dist
+    if distance[step_to] > new_dist:
+      distance[step_to] = new_dist
+      queue.append([new_dist, step_to])
+
+print(distance[N])

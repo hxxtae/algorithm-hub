389b26f860ed7d9e25522ef7ba7b8d99620c1e83
Heetae Kim <fkdlxmfkdl@gmail.com>
2024-06-19 00:30:54 +0900

    [Gold I] Title: 인터넷 설치, Time: 288 ms, Memory: 115632 KB -BaekjoonHub

--- 백준/Gold/1800. 인터넷 설치/인터넷 설치.py
+++ 백준/Gold/1800. 인터넷 설치/인터넷 설치.py
@@ -1,53 +1,54 @@
+from heapq import *
 import sys
-import heapq
-input = sys.stdin.readline
-INF = float('INF')
+read = sys.stdin.readline
 
-def dijkstra(base):
-    q = []
-    heapq.heappush(q, (0, 1))
-    dist = [INF for _ in range(N+1)]
-    dist[1] = 0
+N, P, K = map(int, read().rstrip().split())
+GRAPH = [[*map(int, read().rstrip().split())] for _ in range(P)]
 
-    while q:
-        d, now = heapq.heappop(q)
+# 1. Set Graph
+graph = [[] for _ in range(N+1)]
+for a, b, dist in GRAPH:
+  graph[a].append((dist, b))
+  graph[b].append((dist, a))
 
-        if dist[now] < d:
-            continue
+def onDijkstra(base_cost):
+  # 2. Set Distance
+  distance = [float('inf')] * (N+1) # dist[n]: 1번부터 n번을 연결하는 데 사용된 공짜 케이블 개수 
+  distance[1] = 0
 
-        for (next, nextDist) in graph[now]:
-            cost = d
-            if nextDist > base:
-                cost += 1
+  # 3. BFS
+  heap = [(0, 1)]
+  heapify(heap)
+  while heap:
+    dist, to = heappop(heap)
+    
+    if distance[to] < dist: continue
 
-            if cost < dist[next]:
-                dist[next] = cost
-                heapq.heappush(q, (cost, next))
+    for step_cost, step_to in graph[to]:
+      free_dist = dist # dist: 공짜 케이블 선의 개수
+      if step_cost > base_cost: # 기준 비용보다 큰 비용은 공짜 케이블로 대체하기 위해
+        free_dist += 1 # 공짜 케이블 1개 추가
+      
+      if free_dist < distance[step_to]:
+        distance[step_to] = free_dist
+        heappush(heap, (free_dist, step_to))
 
-    return dist[N] <= K 
-
+  # 1번과 N번을 연결하는 데 K개 이하의 공짜 케이블이 사용된다면
+  # base 가격은 정답이 될 수 있다.
+  return distance[N]
 
 if __name__ == '__main__':
+  left = 0
+  right = 1_000_000
+  answer = -1
+
+  while left <= right:
+    mid = (left + right) // 2
     
-    N, P, K = map(int, input().split())
-    graph = [[] for _ in range(N+1)]
-
-    for _ in range(P):
-        a, b, c = map(int, input().split())
-        graph[a].append((b, c))
-        graph[b].append((a, c))
-
-    left = 0
-    right = 1_000_000
-    res = -1
-
-    while left <= right:
-        mid = (left + right) // 2
-
-        if dijkstra(mid):
-            res = mid
-            right = mid - 1
-        else:
-            left = mid + 1
-
-    print(res)
+    if onDijkstra(mid) <= K:
+      answer = mid
+      right = mid - 1
+    else:
+      left = mid + 1
+  
+  print(answer)

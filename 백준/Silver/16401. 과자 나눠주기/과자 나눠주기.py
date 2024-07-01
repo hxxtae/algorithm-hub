a59c11c921e656410f049b09d48b872e7e97e3c2
Heetae Kim <fkdlxmfkdl@gmail.com>
2024-07-01 12:41:26 +0900

    [Silver II] Title: 과자 나눠주기, Time: 808 ms, Memory: 254036 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
@@ -0,0 +1,27 @@
+import sys
+read = sys.stdin.readline
+
+M, N = map(int, read().rstrip().split())
+snacks = [*map(int, read().rstrip().split())]
+
+start = 1
+end = max(snacks)
+answer = 0
+
+while start <= end:
+  mid = (start + end) // 2
+
+  cnt = 0
+  for length in snacks:
+    while mid <= length:
+      cnt += 1
+      length -= mid
+  
+  if cnt >= M:
+    start = mid + 1
+    answer = mid
+  else:
+    end = mid - 1
+
+print(answer)
+

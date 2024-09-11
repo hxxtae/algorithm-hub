83125b471eb0e5629e1029ac90756ae8925ec025
Heetae Kim <fkdlxmfkdl@gmail.com>
2024-09-11 23:52:06 +0900

    [Silver II] Title: A → B, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,18 @@
+import sys
+read = sys.stdin.readline
+
+a, b = map(int,read().split())
+r = 1
+while(b!=a):
+    r+=1
+    temp = b
+    if b%10 == 1:
+        b//=10
+    elif b%2 == 0:
+        b//=2
+    
+    if temp == b:
+        print(-1)
+        break
+else:
+    print(r)

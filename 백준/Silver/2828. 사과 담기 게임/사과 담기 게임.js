773ec9be928e0c1f706fc89cc0685ec70938cba2
Heetae Kim <fkdlxmfkdl@gmail.com>
2024-09-30 23:56:17 +0900

    [Silver V] Title: 사과 담기 게임, Time: 88 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const input = fs.readFileSync("./dev/stdin").toString().trim().split('\n');
+
+const [N, M] = input.shift().split(' ').map(Number);
+input.shift();
+const apples = input.map(Number)
+
+let left = 1;
+let right = M;
+let move = 0;
+
+for (let i = 0; i < apples.length; i++) {
+  const apple = apples[i];
+  if (apple >= left && apple <= right) continue;
+  else if (apple < left) {
+    const dist = left - apple;
+    left = apple;
+    right = right - dist;
+    move += dist;
+  } else {
+    apple > right
+    const dist = apple - right;
+    right = apple;
+    left = left + dist;
+    move += dist;
+  }
+}
+console.log(move)

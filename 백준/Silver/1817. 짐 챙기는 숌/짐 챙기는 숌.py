877594763a0aa7d21c623b112bd8b043165b5656
Heetae Kim <fkdlxmfkdl@gmail.com>
2024-11-10 00:19:55 +0900

    [Silver V] Title: 짐 챙기는 숌, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1817. 짐 챙기는 숌/짐 챙기는 숌.py
+++ 백준/Silver/1817. 짐 챙기는 숌/짐 챙기는 숌.py
@@ -1,17 +1,17 @@
 import sys
 read = sys.stdin.readline
 
-n,m=map(int,read().split())
-count = 0
-if n == 0 : print(0)
-else : 
-    boxes = list(map(int,read().split()))
-    weight = 0
-    count = 1 
-    for box in boxes:   
-        if box+weight <= m : 
-            weight += box
-        else : 
-            weight = box 
-            count +=1
-    print(count)
+N, M = [*map(int, read().rstrip().split())]
+ARR = [*map(int, read().rstrip().split())]
+
+cnt = 1
+bookSum = 0
+for book in ARR:
+  if (bookSum + book) > M:
+    cnt += 1
+    bookSum = book
+  else:
+    bookSum += book
+
+if N == 0: print(0)
+else: print(cnt)

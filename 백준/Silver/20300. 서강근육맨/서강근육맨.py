baadc819b958713e810e6179a4430a02071d9c3e
Heetae Kim <fkdlxmfkdl@gmail.com>
2024-11-12 23:56:45 +0900

    [Silver III] Title: 서강근육맨, Time: 40 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/20300. 서강근육맨/서강근육맨.py
@@ -0,0 +1,12 @@
+import sys
+input=sys.stdin.readline
+ 
+input()
+a = [int(i) for i in input().split()]
+a.sort()
+m = 0
+if len(a)%2 == 1:
+    m = a.pop(-1)
+b = [a[i] + a[-i-1] for i in range(len(a)//2)]
+b.append(m)
+print(max(b))

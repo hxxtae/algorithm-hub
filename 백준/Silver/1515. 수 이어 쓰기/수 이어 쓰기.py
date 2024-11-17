4f18ec425c581b8cb14a44236a91037ab2b6707c
Heetae Kim <fkdlxmfkdl@gmail.com>
2024-11-18 00:27:37 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
@@ -1,12 +1,15 @@
 import sys
-nums = sys.stdin.readline().rstrip()
+read = sys.stdin.readline
+
+S = read().rstrip()
+i = 0
 n = 0
-idx = 0
+answer = 0
 while True:
-    n += 1
-    for s in str(n):
-        if nums[idx] == s:
-            idx += 1
-            if idx >= len(nums):
-                print(n)
-                exit()
+  n += 1
+  for ss in str(n):
+    if ss == S[i]:
+      i += 1
+      if i == len(S):
+        print(n)
+        exit()

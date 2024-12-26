becb112e6f05fc9de2af70a16bd4cd77ffb2a341
Heetae Kim <fkdlxmfkdl@gmail.com>
2024-12-27 01:02:03 +0900

    [Silver IV] Title: Byte Coin, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/17521. Byte Coin/Byte Coin.py
@@ -0,0 +1,24 @@
+import sys
+read = sys.stdin.readline
+
+n, w = map(int, read().split())
+
+price = []
+for _ in range(n):
+    price.append(int(read()))
+
+amount = 0
+
+for i in range(n-1):
+    if price[i] < price[i+1]:
+        if w // price[i] > 0:
+            amount = w // price[i]
+            w = w - (amount * price[i])
+    elif price[i-1] < price[i] and amount > 0:
+        w = w + amount * price[i]
+        amount = 0
+
+if amount > 0:
+    w = w + amount * price[n-1]
+
+print(w)

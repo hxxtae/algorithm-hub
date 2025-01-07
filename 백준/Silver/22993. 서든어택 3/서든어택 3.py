4fc34431230a72078133f59814f9042333dfe5b6
Heetae Kim <fkdlxmfkdl@gmail.com>
2025-01-08 01:04:39 +0900

    [Silver IV] Title: 서든어택 3, Time: 88 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Silver/22993. 서든어택 3/서든어택 3.py
+++ 백준/Silver/22993. 서든어택 3/서든어택 3.py
@@ -1,15 +1,17 @@
 import sys
 read = sys.stdin.readline
 
-n = int(read())
-array = list(map(int, read().split()))
-attack = array[0]
-array = sorted(array[1:])
-p = True
-for i in array:
-    if attack > i:
-        attack += i
-    else:
-        p = False
-        break
-print('Yes' if p else 'No')
+N = int(read().rstrip())
+ARR = list(map(int, read().rstrip().split()))
+
+def confirmSurvive(arr):
+  A = arr[0]
+  for Ai in sorted(arr[1:N], key=lambda x: x):
+    if A > Ai: 
+      A += Ai
+    else: 
+      return "No"
+    
+  return "Yes"
+
+print(confirmSurvive(ARR))

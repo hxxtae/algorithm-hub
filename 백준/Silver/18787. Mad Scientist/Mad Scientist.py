e907d927e6a22577e2cfe1b3e3bf3be347be47d6
Heetae Kim <fkdlxmfkdl@gmail.com>
2025-03-26 23:43:31 +0900

    [Silver V] Title: Mad Scientist, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/18787. Mad Scientist/Mad Scientist.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = input()
+b = input()
+
+result = 0
+flag = False
+
+for i in range(len(b)) :
+  if flag == True or a[i] != b[i] :
+    flag = True
+  if flag == True and a[i] == b[i] :
+    result += 1
+    flag = False
+
+print(result)

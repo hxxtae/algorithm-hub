de45819fbddccbfe156108b15f3e2a66e3dc9b44
Heetae Kim <fkdlxmfkdl@gmail.com>
2025-04-12 23:47:53 +0900

    [Silver III] Title: 작은 수 내기, Time: 124 ms, Memory: 47096 KB -BaekjoonHub

---
+++ 백준/Silver/16471. 작은 수 내기/작은 수 내기.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+j = list(map(int, input().split()))
+s = list(map(int, input().split()))
+
+j.sort(reverse=True)
+s.sort()
+cnt = 0
+
+for i in j:
+    if i >= s[-1]:
+        pass
+    else:
+        cnt += 1
+        s.pop()
+
+if cnt >= (n+1)/2:
+    print("YES")
+else:
+    print("NO")
